791a8de9020ba81212678a59dac4678a56db2079
fw21-0395 <nrj143official@gmail.com>
2022-11-10 19:16:28 +0530

    Restore

--- scripts/login.js
+++ scripts/login.js
@@ -1,6 +1,6 @@
-import {navbar} from "./components/navbar.js";
-let navbar_div = document.getElementById('navbar');
-navbar_div.innerHTML=navbar();
+// import {nav_part} from "./nav_part.js";
+// let navbar_div = document.getElementById('nav_part');
+// navbar_div.innerHTML=nav_part();
 
 
 
@@ -8,11 +8,6 @@ navbar_div.innerHTML=navbar();
   // console.log("gb")
    document.querySelector("#log").classList.add("active");
    log.style.visibility="visible";
-
-   document.getElementById('l_username').value="";
-  document.getElementById('l_password').value="";
-   document.getElementById('l_email').value="";
-
 });
 
 
@@ -23,64 +18,44 @@ document.querySelector("#log #close-btntwo").addEventListener("click",()=>{
 
 });
 
-
-document.querySelector("body").addEventListener("load",()=>{
-  //console.log("gbb")
-  let data = JSON.parse(localStorage.getItem('login_data'));
-  if(data==null){
-    span.innerText = `user`;
- 
-  }else{
-    span.innerText = data.username;
-  }
-  
-});
-
-
 function usernameChange() {
    
    let span = document.getElementById("open-popuptwo");
-   let data = JSON.parse(localStorage.getItem('login_data'));
-   if(data==null){
-    span.innerText = `user`;
-   
-   }else{
-    span.innerText = data.username;
-   };
+   span.innerText  = `Log in`
+
+
 
    document.getElementById("login").addEventListener("click",(event)=>{
        event.preventDefault()
-
+    
+      
+    
+      
        class user {
        #checkUsername(username){return username.includes('#')? false : true;}
       
      
-        #checkPassword(password){ return password.length > 8 ? true : false;}
+         #checkPassword(password){ return password.length > 8 ? true : false;}
 
         
      
          async  Login(username,password){
             let isValidated = this.#checkUsername(username) && this.#checkPassword(password) ;
               if(isValidated ){
-                this.username=username;
-                this.password=password;
+            this.username=username;
+           // this.email=email;
+            this.password=password;
             
     
             let login_data = (this);
             console.log(this);
             try{
-               if( this.username=="Neeraj" &&  this.password=="aroma_12345"){
-                alert('Admin LogedIn successssfully !!');
-                 window.location="rough-admin.html";    
-
-               }else{
-                localStorage.setItem('login_data',JSON.stringify(login_data ," login_data " ) );
-                alert('User LogedIn successssfully !!');
-                //if need to change the location of window give the next location below 
                
-                 window.location="topoffer.html";   
-               }
-                let data = JSON.parse(localStorage.getItem('login_data'));
+                localStorage.setItem('login_data',JSON.stringify(login_data ," login_data " ) );
+               // let data = JSON.parse(localStorage.getItem('login_data'));
+              
+            alert('User LogedIn successssfully !!');
+            //window.location="index.html";    
             }catch(err){
                 console.log("err");
                 alert('something went wrong !' );
@@ -90,7 +65,7 @@ function usernameChange() {
         };
         };
       }
-     //
+ 
       let u1 =new user();
       Login();
     function Login(){
@@ -101,9 +76,13 @@ function usernameChange() {
         alert("check your data!");
     }else{
         u1.Login(username,password,email);
+        span.innerText = username;
     }
-  
+    
+      
    }
+   
 });
 }
+
 usernameChange();
--- scripts/signup.js
+++ scripts/signup.js
@@ -17,6 +17,8 @@ create.addEventListener('dblclick',function(){
 
 
 class user {
+   
+
     #checkUsername(username){
 
     return username.includes('#')? false : true;
@@ -27,7 +29,7 @@ class user {
     }
 
 
-async  signup(n, l ,u, e, p,z){
+async  signup(n, l ,u, e, p){
 console.log("gauri");
         let isValidated = this.#checkUsername(u) && this.#checkPassword(p);
         if(isValidated){
@@ -36,30 +38,27 @@ console.log("gauri");
             this.username = u;
             this.email = e ;
             this.password = p ;
-            this.zipcode = p ;
-
+            
             console.log(this);
            // let actual_data = JSON.stringify(this);
            let signup_data = (this);
             try{
-                if(this.zipcode.length >=5 ){
-                    // let  data = await res.json();
-                    localStorage.setItem('signup_data',JSON.stringify(signup_data ," signup_data " ) );
-                    alert(`${this.username} Signup successssfully !!`);
-                    window.location="topoffer.html";
-                }else{
-                    alert(`${this.username} Enter valid zipcode`);
-                }
+       
+
+       // let  data = await res.json();
+       localStorage.setItem('signup_data',JSON.stringify(signup_data ," signup_data " ) );
        // let data = JSON.parse(localStorage.getItem('login_data'));
+      
            // console.log('data',data);
+            alert('${username} Signup successssfully !!');
+            window.location="index.html";
+           
+
         }catch(err){
             console.log('err!',err);
         }
         }else{
-            let msg = document.getElementById("msg");
-            msg.innerText="Oops! Something's not right with your submission. Please try again!"; 
-            msg.style.backgroundColor="#f79d9f";
-            msg.style.color="#ed0331";
+            alert('password length should be more than 9!'); 
         };
  };
 
@@ -76,8 +75,7 @@ const last_name = document.getElementById('last_name').value;
 const username = document.getElementById('username').value;
 const email = document.getElementById('email').value;
 const password=document.getElementById('password').value;
-const zipcode=document.getElementById('zipcode').value;
 
-u1.signup(name,last_name,username,email,password,zipcode);
+u1.signup(name,last_name,username,email,password);
 }
 
