668247814e299f5a73a29885c3a616746c6fc620
7r-Rishi <7r.rushi@gmail.com>
2022-11-10 19:30:22 +0530

    cart_page-day-3

---
+++ scripts/cart.js
@@ -0,0 +1,19 @@
+// const getData = () => {
+//     try{
+//         let res = await fetch(``)
+        
+//     }
+//     catch(err){
+//         console.log(err);
+//     }
+// }
+
+
+// const remove_cart_item = document.getElementById('remove_cart_item');
+// remove_cart_item.addEventListener('click',()=>{
+//     remove_item();
+// })
+
+// const remove_item = ()=>{
+//     console.log('inside function')
+// }
