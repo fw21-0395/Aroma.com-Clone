cef9c3ee273a1cd5750433b6178772f7a25239c5
gitusergb <gauribidwai94@gmail.com>
2022-11-10 16:37:38 +0530

    changes day3

--- components/navbar.js
+++ components/navbar.js
@@ -49,6 +49,49 @@ const navbar = () => {
 
                 </div>
                 <i class="fa-solid fa-user"></i>
+
+                <!--login/signup code-->
+    <div id="logoff">
+
+        <div>
+   
+        <img  id="signup" src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.219/on/demandware.static/Sites-BathAndBodyWorks-Site/-/default/dwa626f986/images/svg-icons/UI-MyAccount.svg?yocs=o_s_
+        " alt="sign_in_logo">Hello, <span id="open-popuptwo"></span>
+        
+<div id="log">
+    <div class="close-btntwo" id="close-btntwo">&times;</div>
+    <a class="anc" href="signup.html" title="Sign In or Sign Up">Sign Up</a>
+    <a class="anc" href="index.html" title="My wishlist">My wishlist</a>
+    <a class="anc" href="order-status.html" title="Order Tracking">Order Tracking</a>
+    <a class="anc" href="my_wishlist.html" title="My wishlist">My wishlist</a>
+    
+
+<div id="logtwo">            
+    <div class="inputtwo">
+        <div id="forms">
+            <div id="login-form">
+                     <h5>SIGN IN</h5>
+                        <span>If you already have an account with us, sign in below</span>
+                         <hr>
+                    <form id="form">
+                    <label >Name</label>
+                    <input type="text" id="l_username" placeholder="Username" />
+                    <label >Email Address</label>
+                    <input type="text" id="l_email" placeholder="Email" />
+                    <label>Password</label>
+                    <input type="text" id="l_password" placeholder="Password" />
+                    </form>
+                    <button id="login">SIGN IN</button>
+            </div>
+        </div>
+    </div>
+</div>  
+</div>
+ </div>
+</div>
+<!--login/signup code-->
+               
+     
                 <i class="fa-solid fa-cart-arrow-down"></i>
 
             </div>
@@ -72,6 +115,5 @@ const navbar = () => {
     `
 
 }
-
 //Named Export - Destructured Export
 export { navbar };
---
+++ order.js
@@ -0,0 +1,49 @@
+
+
+
+class user {
+async  signup(n, e,z){
+console.log("gauri");
+    
+            this.order_number=n;
+            this.email = e ;
+            this.zipcode = z ;
+
+            console.log(this);
+           // let actual_data = JSON.stringify(this);
+           let signup_data = (this);
+            try{
+                if(this.zipcode.length ==6 ){
+                    // let  data = await res.json();
+                    localStorage.setItem('signup_data',JSON.stringify(signup_data ," signup_data " ) );
+                    alert(`${this.order_number} Order on the Way !!`);
+                    window.location="index.html";
+                }else{
+                    let msg = document.getElementById("msg");
+                    msg.innerText="Oops! Something's not right with your submission. Please try again!"; 
+                    msg.style.backgroundColor="#f79d9f";
+                    msg.style.color="#ed0331";
+                }
+       // let data = JSON.parse(localStorage.getItem('login_data'));
+           // console.log('data',data);
+        }catch(err){
+            console.log('err!',err);
+        }
+           
+        
+ };
+ 
+
+};
+let u1 =new user();
+
+function order(){
+const order_number = document.getElementById('order_number').value;
+
+const email = document.getElementById('email').value;
+
+const zipcode=document.getElementById('zipcode').value;
+
+u1.signup(order_number,email,zipcode);
+}
+
